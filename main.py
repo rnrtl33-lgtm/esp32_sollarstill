56cc084cfef5c0ba2202a568b8625dd5b28d5609
rnrtl33-lgtm <rnrtl33@gmail.com>
2025-12-02 08:46:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+print("=== ESP32 MAIN.PY STARTED ===")
+print("This is a test version from GitHub.")
+print("If you see this message, the OTA pull worked successfully!")
+
+import time
+
+counter = 0
+while True:
+    counter += 1
+    print("Running test loop...  Count =", counter)
+    time.sleep(2)
 
