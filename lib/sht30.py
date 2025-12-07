be7bb70f522140872cbe7ffb4c661f578b977626
rnrtl33-lgtm <rnrtl33@gmail.com>
2025-12-07 12:16:57 +0300

    Update sht30.py

--- lib/sht30.py
+++ lib/sht30.py
@@ -1,21 +1,23 @@
-# MicroPython SHT30 Driver
 import time
 
 class SHT30:
-    def __init__(self, i2c, addr=0x44):
+    def __init__(self, i2c, address=0x44):
         self.i2c = i2c
-        self.addr = addr
+        self.addr = address
 
-    def read(self):
+    def measure(self):
+        # Send measurement command
         self.i2c.writeto(self.addr, b'\x2C\x06')
-        time.sleep(0.015)
+        time.sleep_ms(15)
+
         data = self.i2c.readfrom(self.addr, 6)
+        if len(data) != 6:
+            return None, None
 
-        temp_raw = data[0] << 8 | data[1]
-        hum_raw = data[3] << 8 | data[4]
+        t_raw = data[0] << 8 | data[1]
+        h_raw = data[3] << 8 | data[4]
 
-        temp = -45 + (175 * (temp_raw / 65535))
-        hum = 100 * (hum_raw / 65535)
+        temp = -45 + (175 * (t_raw / 65535))
+        hum  = 100 * (h_raw / 65535)
 
-        return temp, hum
-
+        return round(temp, 2), round(hum, 2)
