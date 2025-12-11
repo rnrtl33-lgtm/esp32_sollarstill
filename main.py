087f83dd0eb806707907e35f44999fae10bc7727
rnrtl33-lgtm <rnrtl33@gmail.com>
2025-12-11 16:14:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("MAIN STARTED v2")
+import time
+import urequests
+
+print("MAIN STARTED - THINGSPEAK TEST")
+
+API_KEY = "اكتب_مفتاح_القناة_التي_تريد_الاختبار_عليها"
+counter = 0
+
+while True:
+    counter += 1
+    url = "https://api.thingspeak.com/update?api_key={}&field1={}".format(API_KEY, counter)
+    
+    try:
+        r = urequests.get(url)
+        print("Sent:", counter, " → TS:", r.text)
+        r.close()
+    except Exception as e:
+        print("ERROR sending:", e)
+    
+    time.sleep(15)
+
